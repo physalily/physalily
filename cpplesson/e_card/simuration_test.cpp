29c8db00ad692ff44d5eaaf3bb108bc631197c23
physalily <physalily@gmail.com>
2021-05-07 23:15:35 +0900

    [fix][cpp] e card simulator

--- cpplesson/e_card/simuration_test.cpp
+++ cpplesson/e_card/simuration_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
+#include <random>
 
 int main()
 {
@@ -15,12 +16,19 @@ int main()
 	int slave_card = 0;
 	int king_card = 0;
 
+	// random setup
+	// seed settings
+	std::random_device seed_gen;
+	std::default_random_engine engine(seed_gen());
+	// generate random number 1 to 5
+	std::uniform_int_distribution<> dist(1, 5);
+
 	// main loop
 	for (size_t i = 0; i < simulation_iter; i++) {
-		srand(time(NULL));
-		slave_card = rand() % 5 + 1;
-		srand(time(NULL));
-		king_card = rand() % 5 + 1;
+		slave_card = dist(engine);
+		king_card = dist(engine);
+		std::cout << slave_card << std::endl;
+		std::cout << king_card << std::endl;
 		if (slave_card == 1 && king_card == 5)
 			slave_win++;
 		else if(slave_card < king_card)
@@ -33,7 +41,7 @@ int main()
 	std::cout << "slave win: " << slave_win << std::endl;
 	std::cout << " king win: " << king_win  << std::endl;
 	if (slave_win == 0) {
-		std::cout << "zero d error.";
+		std::cout << "zero d error." << std::endl;
 		return 0;
 	}
 	std::cout << " probability : " << (double)slave_win / (double)simulation_iter 
