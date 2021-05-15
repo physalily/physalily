54782328756aadc37a431a58c90277f13fb57a1b
physalily <physalily@gmail.com>
2021-05-15 20:19:51 +0900

    [add][c__] e card game in cli

---
+++ cpplesson/e_card/cli.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Game start." << std::endl;
+	while (true)
+	{
+		int role = 0;
+		std::cout << "Select your role. king:1 or slave:2" << std::endl;
+		std::cin >> role;
+		// do game
+		break;
+	}
+	return 0;
+}
+
